ce136456c27830e6f6b2213a831660bb6d88c469
donfrigo <donfrigo@gmail.com>
2018-10-17 21:58:45 +0200

    Updates

---
+++ precache-manifest.e72c3088543cf544c6b00ab63f256af9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f06469f468e5b35f41be",
+    "url": "/open-multiple-links/static/js/runtime~main.f06469f4.js"
+  },
+  {
+    "revision": "3658bde796b254349073",
+    "url": "/open-multiple-links/static/js/main.3658bde7.chunk.js"
+  },
+  {
+    "revision": "2dafdcf0f2e33a425327",
+    "url": "/open-multiple-links/static/js/1.2dafdcf0.chunk.js"
+  },
+  {
+    "revision": "3658bde796b254349073",
+    "url": "/open-multiple-links/static/css/main.5f9cdc23.chunk.css"
+  },
+  {
+    "revision": "74a79adb48da82103f6bd896fd50453d",
+    "url": "/open-multiple-links/index.html"
+  }
+];
